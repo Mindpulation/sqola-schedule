633c2a720733e07b0db26354c89b2ec2bb09e73a
Reza Andriansyah <technoboth@gmail.com>
2020-09-22 02:54:41 +0700

    add http module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import http from 'http';
 import cors from 'cors';
 
 import sec from './env/index.js';
@@ -8,4 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(sec.PORT);
+
+
+const serv = http.createServer(app);
+serv.listen(sec.PORT);
