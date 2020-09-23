fa10c1b6db53616d65cc7cbd29d8a9a6c0b20b76
Reza Andriansyah <technoboth@gmail.com>
2020-09-23 19:19:18 +0700

    update syntax

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
+
+import find from './view/find.js';
 import sec from './env/index.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/find', find);
+
+app.all("*", (req, res)=>{res.send({res:"Fuck you!"})});
 
 const serv = http.createServer(app);
 serv.listen(sec.PORT);
